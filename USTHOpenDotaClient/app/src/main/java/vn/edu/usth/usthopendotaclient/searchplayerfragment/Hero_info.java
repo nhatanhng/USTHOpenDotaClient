7a572d0087bc58d3b85b76a94ddabdce802214bf
Tranze <anhtd.bi12-018@st.usth.edu.vn>
2023-09-19 00:18:32 +0700

    new addition fragment

--- USTHOpenDotaClient/app/src/main/java/vn/edu/usth/usthopendotaclient/searchplayerfragment/Hero_info.java
+++ USTHOpenDotaClient/app/src/main/java/vn/edu/usth/usthopendotaclient/searchplayerfragment/Hero_info.java
@@ -1,20 +1,25 @@
 package vn.edu.usth.usthopendotaclient.searchplayerfragment;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import vn.edu.usth.usthopendotaclient.R;
 
-public class Matches_History_Fragment extends Fragment {
+
+public class Hero_info extends Fragment {
+    private LinearLayout mlinearLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_matches_history, container, false);
+        View v = inflater.inflate(R.layout.hero_info, container, false);
         return v;
     }
 }
