f02040f974f862768414992ec498d4624bbbadd7
nhatanhng <nguyennhatanh0810@gmail.com>
2023-09-18 10:12:39 +0700

    add OverviewFragment for Search

---
+++ USTHOpenDotaClient/app/src/main/java/vn/edu/usth/usthopendotaclient/searchplayerfragment/OverviewFragment.java
@@ -0,0 +1,28 @@
+package vn.edu.usth.usthopendotaclient.searchplayerfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.edu.usth.usthopendotaclient.R;
+
+
+public class OverviewFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+}
